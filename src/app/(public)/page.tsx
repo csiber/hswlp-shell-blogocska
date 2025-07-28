052b8f5946a202b7cf6e578d453f6032c1b0de1b
Csaba Polyak <csiberius@gmail.com>
2025-07-28 13:51:12 +0200

    feat(blog): improve feed layout

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,7 +4,7 @@ import NavFooterLayout from "@/layouts/NavFooterLayout";
 export default function Page() {
   return (
     <NavFooterLayout>
-      <div className="container mx-auto py-6">
+      <div className="container mx-auto flex justify-center py-6">
         <BlogFeed />
       </div>
     </NavFooterLayout>
