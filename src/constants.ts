90ac0d4f30648ffe09994e0feaf1ee861f4726cb
Csaba Polyak <csiberius@gmail.com>
2025-07-28 10:16:36 +0200

    Add credits expiration constant

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,7 @@ export const GITHUB_REPO_URL = "https://github.com/csiber/hswlp-next"
 export const SITE_DOMAIN = new URL(SITE_URL).hostname
 export const PASSWORD_RESET_TOKEN_EXPIRATION_SECONDS = 24 * 60 * 60 // 24 hours
 export const EMAIL_VERIFICATION_TOKEN_EXPIRATION_SECONDS = 24 * 60 * 60 // 24 hours
+export const CREDITS_EXPIRATION_YEARS = 1
 export const MAX_SESSIONS_PER_USER = 5;
 export const SESSION_COOKIE_NAME = "session";
 export const REDIRECT_AFTER_SIGN_IN = "/dashboard" as Route;
