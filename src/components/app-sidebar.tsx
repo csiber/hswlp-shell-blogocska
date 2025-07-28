620c37416b3b607244e61d897b6109aa7f837ac8
Csaba Polyak <csiberius@gmail.com>
2025-07-28 10:06:57 +0000

    0.0.0.7

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -12,7 +12,6 @@ import {
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
 import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
@@ -107,7 +106,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
       </SidebarContent>
       <SidebarFooter>
         <NavUser />
